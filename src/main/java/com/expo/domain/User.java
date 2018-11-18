f1e0acf9c95c435d45e25da45ad70c49c535d668
liukai <liukai@vipkid.com.cn>
2018-11-18 11:56:29 +0800

    fi

---
+++ src/main/java/com/expo/domain/User.java
@@ -0,0 +1,12 @@
+package com.expo.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by liukai on 2018/11/18.
+ */
+@Setter
+@Getter
+public class User {
+}
---
+++ src/main/java/com/expo/service/Impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.expo.service.Impl;
+
+/**
+ * Created by liukai on 2018/11/18.
+ */
+public class UserServiceImpl {
+}
